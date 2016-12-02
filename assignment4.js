82658e0f3e26151112d0221588c19c68d5816f9a
Salvatore Avena <sja33@pitt.edu>
2016-12-01 20:50:34 -0500

    Initial Commit

--- assignment4.js
+++ assignment4.js
@@ -11,7 +11,61 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
+var APIurl = 'http://www.mattbowytz.com/simple_api.json?data=all';
+var code;
+var data;
+var info;
+var status;
+var interests;
+var programming;
+
 (function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
+
+  $.getJSON(APIurl, function(x){
+    code = x.code;
+    data = x.data;
+    info = x.info;
+    status = x.status;
+    interests = data.interests;
+    programming = data.programming;
+    console.log(x);
+    console.log(code);
+    console.log(info);
+    console.log(status);
+    console.log(interests);
+    console.log(programming);
+  })
+
+  $('.flexsearch-input').keyup(function(){
+    var output = "<p id=resultsHeader>Displaying results for <em>&quot;"
+    var searchTerm = $('.flexsearch-input').val();
+    searchTerm = searchTerm.toLowerCase();
+    output = output.concat(searchTerm + "&quot;</em>:</p>");
+    if (searchTerm.length > 0) {
+      $('.results').html(output);
+      $('.results').append("<b>Results from Interests:</b>");
+      $('.results').append("<hr>");
+      for (var i=0; i < interests.length; i++) {
+        var interest = interests[i];
+        if(searchTerm == interest.substring(0, searchTerm.length).toLowerCase()){
+          interestplus = interest.replace(" ","+");
+          $('.results').append('<br><a href=http://www.google.com/search?q='+interestplus+'>'+interest+'</a>');
+        }
+      };
+      $('.results').append("<br></br><b>Results from Programs:</b>");
+      $('.results').append("<hr>");
+      for (var i=0; i < programming.length; i++) {
+        var program = programming[i];
+        if(searchTerm == program.substring(0, searchTerm.length).toLowerCase()){
+          programplus = program.replace(" ","+");
+          $('.results').append('<br><a href=http://www.google.com/search?q='+programplus+'>'+program+'</a>');
+        }
+      };
+
+    }
+    else {
+      $('.results').html("");
+    }
+  });
+
 })();
